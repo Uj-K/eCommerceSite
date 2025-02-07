3676b5ef39c88ea9a07488d465b888d067c2d3fb
Uj-K <umkrall@gmail.com>
2025-02-07 15:03:04 -0800

    Added DB Context class

---
+++ eCommerceSite/Data/MusicInstrumentContext.cs
@@ -0,0 +1,15 @@
+﻿using eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace eCommerceSite.Data
+{
+    public class MusicInstrumentContext : DbContext
+    {
+        public MusicInstrumentContext(DbContextOptions<MusicInstrumentContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<MusicInstrument> MusicInstruments { get; set; }
+    }
+}
