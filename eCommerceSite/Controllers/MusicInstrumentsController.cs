d3913a59639a3c9268cb40a33bfe2b99d57e99d6
Uj-K <umkrall@gmail.com>
2025-02-10 23:19:23 -0800

    Saving to fix the main issue

--- eCommerceSite/Controllers/MusicInstrumentsController.cs
+++ eCommerceSite/Controllers/MusicInstrumentsController.cs
@@ -1,13 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Text;
 
 namespace eCommerceSite.Controllers
 {
     public class MusicInstrumentsController : Controller
     {
+        private readonly MusicInstrumentContext
+
+        public MusicInstrumentsController()
+        {
+                
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(MusicInstrument newMusIns) 
+        {
+            if (ModelState.IsValid) 
+            {
+                // Add to DB
+                // success message
+                return View();
+            }
+
+            return View(newMusIns);
+
         }
     }
 }
