f96db70d9876557aa6a8b6a4109799e98cf937d1
Uj-K <umkrall@gmail.com>
2025-02-14 09:44:04 -0800

    Update is functional

--- eCommerceSite/Controllers/MusicInstrumentsController.cs
+++ eCommerceSite/Controllers/MusicInstrumentsController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerceSite.Data;
 using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using System.Drawing.Text;
 
@@ -51,6 +52,8 @@ namespace eCommerceSite.Controllers
 
         }
 
+
+        [HttpGet]
         public async Task<IActionResult> Edit(int id) 
         {
             MusicInstrument instToEdit = await _context.MusicInstruments.FindAsync(id);
@@ -64,5 +67,19 @@ namespace eCommerceSite.Controllers
         }
         /* async는 비동기 코드는 작업을 실행하고 기다리는 동안 다른 작업을 수행할 수 있는 코드
          * 추가할때 async, Task<>, await 세개가 세트 인듯 */
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(MusicInstrument instModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.MusicInstruments.Update(instModel);
+                await _context.SaveChangesAsync();
+                /*ViewData["Message"] = $"{instModel.Title} was successfully edit!";*/
+                return RedirectToAction("Index");
+
+            }
+            return View(instModel);
+        }
     }
 }
