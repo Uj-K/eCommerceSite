7902742c198d1cf09fcd5d4c64423c8042b3008c
Uj-K <umkrall@gmail.com>
2025-02-24 18:14:25 -0800

    Add member class and DbContext

--- eCommerceSite/Data/MusicInstrumentContext.cs
+++ eCommerceSite/Data/MusicInstrumentContext.cs
@@ -11,5 +11,12 @@ namespace eCommerceSite.Data
         }
 
         public DbSet<MusicInstrument> MusicInstruments { get; set; }
+
+        /*
+         * If want to something store in database, you need to add DbSet property for that class
+         * It tells Entity Framework we need to keep track of these objects in the database
+         */
+        public DbSet<Member> Members { get; set; }
+        
     }
 }
